c4fae26414e6e23148fd4a558aa81d3b370075f7
Manan837 <89606852+Manan837@users.noreply.github.com>
2023-07-18 13:52:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+song="";
+song1="";
+
+function preload()
+{
+    loadSound("music.mp3");
+    loadSound("music2.mp3");
+}
+
 function setup()
 {
-    canvas = createCanvas(600,530);
+    canvas = createCanvas(600,500);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
 }
